cb0c0f473d5d36fd1dbf611a738b4689a0579643
Bruno Oliveira <bruno@abstractj.org>
2016-09-27 06:50:21 -0300

    Force docker compose to respect the order of containers

--- keycloak-sssd-integration-tests/scripts/entrypoint.sh
+++ keycloak-sssd-integration-tests/scripts/entrypoint.sh
@@ -1,10 +1,21 @@
 #!/bin/sh
 
+set -e
+
 DOMAIN=test.local
 IPA=ipa.$DOMAIN
 
-/opt/jboss/scripts/wait-for-it.sh --timeout=0 $IPA:53 --strict -- echo "IPA server is up!" &&
-/opt/jboss/scripts/ipa-client-enroll &&
+until /opt/jboss/scripts/ipa-client-enroll; do
+  >&2
+  echo "-------------------------------------"
+  echo "FreeIPA is unavailable - sleeping"
+  echo "-------------------------------------"
+  sleep 1
+done
+
+echo "-------------------------------------" &&
+echo "IPA server is up and client enrolled!" &&
+echo "-------------------------------------" &&
 /opt/jboss/scripts/ipa-populate-data &&
 /opt/jboss/scripts/ipa-sssd-setup &&
-/opt/jboss/scripts/keycloak-integration-tests && /bin/zsh
+/opt/jboss/scripts/keycloak-integration-tests
