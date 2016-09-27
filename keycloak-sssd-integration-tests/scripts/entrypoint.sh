2d4f009b18315f36515511f42304158e5ab5e183
Bruno Oliveira <bruno@abstractj.org>
2016-09-27 06:50:21 -0300

    Changing the entrypoint

--- keycloak-sssd-integration-tests/scripts/entrypoint.sh
+++ keycloak-sssd-integration-tests/scripts/entrypoint.sh
@@ -3,8 +3,15 @@
 DOMAIN=test.local
 IPA=ipa.$DOMAIN
 
-/opt/jboss/scripts/wait-for-it.sh --timeout=0 $IPA:53 --strict -- echo "IPA server is up!" &&
-/opt/jboss/scripts/ipa-client-enroll &&
+/opt/jboss/scripts/ipa-client-enroll
+while [ $? -eq 1 ]
+do
+  sleep 1
+  /opt/jboss/scripts/ipa-client-enroll
+done
+echo "-------------------------------------" &&
+echo "IPA server is up and client enrolled!" &&
+echo "-------------------------------------" &&
 /opt/jboss/scripts/ipa-populate-data &&
 /opt/jboss/scripts/ipa-sssd-setup &&
 /opt/jboss/scripts/keycloak-integration-tests && /bin/zsh
